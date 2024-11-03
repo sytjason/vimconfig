11290cc48fff5bf9f5b3c6d4d01ca6fcf858b763
sytjason <jasont1020508@gmail.com>
2024-11-03 18:18:27 +0800

    fix toggle linenum

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,6 +2,16 @@ local function map (mode, lhs, rhs)
   vim.keymap.set(mode, lhs, rhs, {})
 end
 
+local function toggleLinenum()
+  if vim.o.nu and vim.o.rnu then
+    vim.o.nu = false
+    vim.o.rnu = false
+  else
+    vim.o.nu = true
+    vim.o.rnu = true
+  end
+end
+
 -- Navigations between panes
 map('n', '<C-j>', '<C-W>j')
 map('n', '<C-k>', '<C-W>k')
@@ -41,4 +51,4 @@ map('n', '<leader>rn', '<cmd>lua require("renamer").rename()<cr>')
 map('v', '<leader>rn', '<cmd>lua require("renamer").rename()<cr>')
 
 -- toggle line num
-map('n', '<leader>nu', ':set rnu!<cr>')
+map('n', '<leader>nu', toggleLinenum)
