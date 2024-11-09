d4698d217402c3b98857b70c76acfacc8677411f
sytjason <jasont1020508@gmail.com>
2024-11-09 17:41:24 +0800

    update

--- lua/toggleterm-setup.lua
+++ lua/toggleterm-setup.lua
@@ -2,7 +2,7 @@ local status_ok, _toggle_term = pcall(require, "toggleterm")
 if not status_ok then return end
 
 _toggle_term.setup{
-  direction = "float",
+  direction = "horizontal",
   open_mapping = [[<F4>]],
 }
 
