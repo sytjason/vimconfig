028b6e3f17f72935c5e6a1c0340c31f9e8ab3e4c
jason_s_tu <jasont1020508@gmail.com>
2025-05-02 15:35:14 +0800

    change copilot accept key

--- lua/copilotchat-setup.lua
+++ lua/copilotchat-setup.lua
@@ -3,4 +3,10 @@ if not status_ok then
   return
 end
 
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+
 copilotchat.setup{}
